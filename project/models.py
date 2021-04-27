6831b5efb551d88a732ce0876e4df906793633c8
Cristian Pintor <cpintor@hawk.iit.edu>
2021-04-27 00:10:32 -0500

    Adding images to posts

--- project/models.py
+++ project/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from PIL import Image
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_pics')
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
